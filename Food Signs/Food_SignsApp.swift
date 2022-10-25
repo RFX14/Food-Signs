220cc6343d20eae4801e814da4832dfbff9322e3
coffeemate <jrosa1756@gmail.com>
2022-10-25 18:40:42 -0500

    Initial Commit

---
+++ Food Signs/Food_SignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_SignsApp.swift
+//  Food Signs
+//
+//  Created by Josue Rosales on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Food_SignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
